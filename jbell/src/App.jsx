e62af4b947da70242348c5ef23ecea7f74989f1e
j89465137-svg <j89465137@gmail.com>
2025-12-31 11:49:00 +0900

    [turtleNeck-KMJ] App.jsx 수정 detail: <UserMap /> 태그 추가

--- jbell/src/App.jsx
+++ jbell/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import UserLayout from './layouts/user/UserLayout';
+import UserMap from './layouts/user/UserMap';
 
 function App() {
   return (
     <>
       <UserLayout />
+      <UserMap />
     </>
   )
 }
