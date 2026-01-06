ac7eb37e0a8288dc8e0cf0a30184be51d4ecdfc5
shykimchi <kwjdgns2198@gmail.com>
2026-01-06 10:01:37 +0900

    [turtleNeck-KJH]  헤더 타이틀 하위메뉴 추가 및 경로 지정 detail : 재난사고속보(홍수, 산사태, 산불), 열린마당(공지사항, 보도자료, 시민안전교육), 고객센터(FAQ, 1대1문의)

--- jbell_frontend/src/routes/user/navigationItems.jsx
+++ jbell_frontend/src/routes/user/navigationItems.jsx
@@ -5,7 +5,10 @@ const navigationItems = [
     children: [ // 하위 메뉴 추가
       { label: "지진", path: "/disaster/earthquake" },
       { label: "태풍", path: "/disaster/typhoon" },
+      { label: "호우", path: "/disaster/heavyRain" },
       { label: "홍수", path: "/disaster/flood" },
+      { label: "산사태", path: "/disaster/landslide" },
+      { label: "산불", path: "/disaster/wildfire" },
     ]
   },
   {
@@ -14,7 +17,7 @@ const navigationItems = [
     children: [
       { label: "자연재난행동요령", path: "/earthquakeActionGuide" },
       { label: "사회재난행동요령", path: "/trafficAccidentActionGuide" },
-      { label: "생활안전행동요령", path: "/listBeforeAction" },
+      { label: "생활안전행동요령", path: "/firstAidActionGuide" },
     ]
   },
   {
@@ -33,10 +36,19 @@ const navigationItems = [
   {
     label: "열린마당",
     path: "/userNoticeList",
+    children: [
+      { label: "공지사항", path: "/userNoticeList" },
+      { label: "보도자료", path: "/userPressRelList" },
+      { label: "시민안전교육", path: "/userSafetyEducation" },
+    ]
   },
   {
     label: "고객센터",
     path: "/faq",
+    children: [
+      { label: "FAQ", path: "/faq" },
+      { label: "1대1 문의", path: "/qna" },
+    ]
   },
 ];
 
